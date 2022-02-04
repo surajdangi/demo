67d2879fdc4c1c8ae8dd3072a2c59ea72eacc6a7
surajdangi <suraj.digiuniv@gmail.com>
2022-02-04 19:00:48 +0530

    commit

---
+++ less_than10.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf( "Enter a number less than 10");
+    scanf("%d",&num);
+
+    if(num<10)
+    printf("my namse is suraj !\n");
+
+    return 0;
+}
---
+++ tot_expens.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int qty,dis=0;
+    float rate,tot;
+    printf ("enter quantity and rate");
+    scanf("%d %f", &qty,&rate);
+    if ( qty > 1000)
+       dis=10;
+
+       tot=(qty*rate)-(qty*rate*dis/100);
+       printf("total expenses = Rs.%f\n", tot);
+       return 0;
+}
